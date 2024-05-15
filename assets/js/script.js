3b8993a60720fd6f554f5686dc428eb2bafc22a5
Daniel Correia <danieljscorreia10@gmail.com>
2024-05-15 18:03:33 -0300

    .

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+//dom
+
+const peso = document.querySelector('#weight')
+const altura = document.querySelector('#height')
+const botao = document.querySelector('#calculate')
+const resultado = document.querySelector('#resultado')
+
+//evento/funcao
+
+botao.addEventListener('click',()=>{
+
+    event.preventDefault()
+
+    p = peso.value
+    a = altura.value/100
+    
+    imc = p/(a**2)
+
+    resultado.textContent = `O seu IMC é: ${imc.toFixed(2)}` 
+
+    if(imc < 14){
+        
+    }
+
+})
+
